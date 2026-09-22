e39515d1dd74dc79c6ba27ad2eef6e5a9f268cef
Marcel781 <39533676+Miraizito781@users.noreply.github.com>
2026-09-22 11:49:43 -0300

    Delete Python8.py

--- Python8.py
+++
@@ -1,6 +0,0 @@
-alunos = 35
-media_Turma = 7.8
-aprovada_Turma = True
-print("O número de alunos é: ", alunos)
-print("A média de nota da turma é: ", media_Turma)
-print("A turma foi aprovada?: ", aprovada_Turma)
